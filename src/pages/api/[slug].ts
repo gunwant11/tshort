214c3cfda8cc1fd5856bbe80efbf6112cb545d68
Gunwant Wankhede <Gunwant@wankhede.me>
2022-11-12 22:20:32 +0530

    added next api

---
+++ src/pages/api/[slug].ts
@@ -0,0 +1,31 @@
+
+import { NextApiRequest, NextApiResponse } from "next";
+import { prisma } from "../../db/client";
+
+export default async (req: NextApiRequest, res: NextApiResponse) => {
+
+    const slug = req.query["slug"];
+
+    if (!slug || typeof slug !== "string") {
+        res.status(400).json({ error: "Missing slug" });
+        return;
+    }
+
+    console.log("slug", slug);
+
+    const data = await prisma.shortURL.findFirst({
+        where: {
+            slug: {
+                equals: slug,
+            }
+        }
+    });
+
+    if (!data) {
+        res.status(404).json({ error: "Not found" });
+        return;
+    }
+
+    return res.redirect(`${data.url}`);
+
+}
